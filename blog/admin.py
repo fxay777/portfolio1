deb68eeb1eb3fa133839ff55de91d1d219779552
fxay777 <jacksonfrancisco09@gmail.com>
2025-08-18 16:57:42 -0300

    Adiciona Post ao Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from .models import Post, PortfolioProject
 
-# Register your models here.
-from .models import Post
-
-from django.contrib import admin
-from .models import PortfolioProject
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "autor", "criado_em")  # ajuste os nomes dos campos conforme estão no seu models.py
+    search_fields = ("titulo", "conteudo")
+    list_filter = ("criado_em", "autor")
 
 @admin.register(PortfolioProject)
 class PortfolioProjectAdmin(admin.ModelAdmin):
