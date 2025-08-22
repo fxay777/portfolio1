da314c8e72e7b57a4ad0184f6e89d7ea16464131
fxay777 <jacksonfrancisco09@gmail.com>
2025-08-22 11:44:24 -0300

    Atualizando

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include   # precisa do include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Inclui as rotas do app blog
+    path('blog/', include('blog.urls')),
 ]
+
