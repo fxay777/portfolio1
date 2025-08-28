fd8f10c8ecceb5fa45fceca305dcd186c658d859
fxay777 <jacksonfrancisco09@gmail.com>
2025-08-28 12:55:55 -0300

    Adiciona templates base.html e index.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-
-from django.http import HttpResponse
-
-def post_view(request):
-    return HttpResponse("Hello World")
+def home(request):
+    return render(request, "blog/index.html")
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),  # envia /blog/ para o app blog
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),  # redireciona rota principal pro blog
 ]
